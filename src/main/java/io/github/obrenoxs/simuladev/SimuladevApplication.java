5760c30cc1bb372c7e60e6148eacc623e8e29d1f
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-10 10:40:05 -0300

    Project created

---
+++ src/main/java/io/github/obrenoxs/simuladev/SimuladevApplication.java
@@ -0,0 +1,13 @@
+package io.github.obrenoxs.simuladev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/obrenoxs/simuladev/SimuladevApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.obrenoxs.simuladev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
