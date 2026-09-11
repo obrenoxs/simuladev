5f15139f8f00578e6213bbb5c0deaf3a26f574ed
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-10 22:33:39 -0300

    feat: create UserRepository with findByEmail query method

---
+++ src/main/java/io/github/obrenoxs/simuladev/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package io.github.obrenoxs.simuladev.user.repository;
+
+import io.github.obrenoxs.simuladev.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+}
