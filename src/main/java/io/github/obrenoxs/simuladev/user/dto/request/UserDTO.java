27ca7c5a5b1bd60e93f542320f329f1f824efe89
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-11 18:39:26 -0300

    feat: create UserDTO with validation constraints

---
+++ src/main/java/io/github/obrenoxs/simuladev/user/dto/request/UserDTO.java
@@ -0,0 +1,21 @@
+package io.github.obrenoxs.simuladev.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserDTO(
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(max = 250)
+    String name,
+
+    @NotBlank(message = "E-mail é obrigatório")
+    @Email(message = "E-mail inválido")
+    @Size(max = 254)
+    String email,
+
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 8, max = 64, message = "Senha deve ter entre 8 e 64 caracteres")
+    String password
+) {}
