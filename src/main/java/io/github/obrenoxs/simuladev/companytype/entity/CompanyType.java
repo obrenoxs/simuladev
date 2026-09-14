c4a49daed8dfedbb3b83882e5709e785abf78798
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-14 10:42:35 -0300

    feat: create CompanyType entity

---
+++ src/main/java/io/github/obrenoxs/simuladev/companytype/entity/CompanyType.java
@@ -0,0 +1,49 @@
+package io.github.obrenoxs.simuladev.companytype.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "company_types")
+public class CompanyType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String domainName;
+    private String description;
+
+    public CompanyType() {
+    }
+
+    public CompanyType(UUID id, String domainName, String description) {
+        this.id = id;
+        this.domainName = domainName;
+        this.description = description;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public void setDomainName(String domainName) {
+        this.domainName = domainName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
