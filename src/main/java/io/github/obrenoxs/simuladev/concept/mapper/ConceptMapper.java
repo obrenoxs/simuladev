53c47b1df021e7f2e0060cabce2601fe19702784
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-16 10:47:19 -0300

    feat: create ConceptMapper

---
+++ src/main/java/io/github/obrenoxs/simuladev/concept/mapper/ConceptMapper.java
@@ -0,0 +1,11 @@
+package io.github.obrenoxs.simuladev.concept.mapper;
+
+import io.github.obrenoxs.simuladev.concept.dto.response.ConceptResponse;
+import io.github.obrenoxs.simuladev.concept.entity.Concept;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ConceptMapper {
+
+    ConceptResponse toResponse(Concept concept);
+}
