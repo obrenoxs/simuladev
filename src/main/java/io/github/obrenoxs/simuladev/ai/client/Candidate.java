a8d52eb786169a7e4d556b0f3347392ac06d6882
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-24 19:09:15 -0300

    feat: create GeminiResponse and Candidate DTOs

---
+++ src/main/java/io/github/obrenoxs/simuladev/ai/client/Candidate.java
@@ -0,0 +1,3 @@
+package io.github.obrenoxs.simuladev.ai.client;
+
+public record Candidate(Content content) {}
---
+++ src/main/java/io/github/obrenoxs/simuladev/ai/client/GeminiResponse.java
@@ -0,0 +1,6 @@
+package io.github.obrenoxs.simuladev.ai.client;
+
+import java.util.List;
+
+public record GeminiResponse(List<Candidate> candidates) {
+}
