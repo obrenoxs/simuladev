60204f3322530ba3e84168344e6e42b93a32e2b2
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-24 20:40:01 -0300

    feat: create RefreshTokenRepository with findAllByUserId

--- src/main/java/io/github/obrenoxs/simuladev/auth/refresh/repository/RefreshTokenRepository.java
+++ src/main/java/io/github/obrenoxs/simuladev/auth/refresh/repository/RefreshTokenRepository.java
@@ -3,7 +3,10 @@ package io.github.obrenoxs.simuladev.auth.refresh.repository;
 import io.github.obrenoxs.simuladev.auth.refresh.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+
+    List<RefreshToken> findAllByUserId(UUID userId);
 }
