5de1f926ad6fb1607bbb64ff07bf444a2aca0242
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-25 09:59:58 -0300

    feat: add findByHashToken to RefreshTokenRepository

--- src/main/java/io/github/obrenoxs/simuladev/auth/refresh/repository/RefreshTokenRepository.java
+++ src/main/java/io/github/obrenoxs/simuladev/auth/refresh/repository/RefreshTokenRepository.java
@@ -4,9 +4,12 @@ import io.github.obrenoxs.simuladev.auth.refresh.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
 
     List<RefreshToken> findAllByUserId(UUID userId);
+
+    Optional<RefreshToken> findByHashToken(String hash);
 }
